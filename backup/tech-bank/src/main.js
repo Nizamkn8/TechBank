de8fff634fb16c05dea1bb5992b96a96fe597658
nizam <nizambinnazar@gmail.com>
2025-11-03 10:58:09 +0530

    remove backup from git

--- backup/tech-bank/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import './assets/tailwind.css'
-import './assets/styles/global.css'
-import './assets/styles/colors/colors.css'
-
-createApp(App).mount('#app')
